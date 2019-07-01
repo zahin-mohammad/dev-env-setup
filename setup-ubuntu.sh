1d48c9fe04d06c9b46e4dbe3af46c0ae9c1b3229
Zahin Mohammad <zahin.dev@gmail.com>
2019-06-30 23:42:46 -0400

    Added pdftx

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -2,37 +2,35 @@ cd $HOME
 
 sudo apt-get update && sudo apt-get upgrade
 
-echo "\e[45m Installing git"
-sudo apt-get install git
-	
-echo "\e[45m Installing curl \e[0m"
-sudo apt-get install curl
-
 echo "\e[45m Installing snap and snapd"
 sudp apt-get install snap && sudo apt-get install snapd
 
-echo "\e[45m Installing zsh \e[0m"
+echo "\e[45m Installing git"
+sudo apt-get install git
 
+echo "\e[45m Installing pdftx"
+sudo snap install pdftk
+
+echo "\e[45m Installing curl \e[0m"
+sudo apt-get install curl
+
+echo "\e[45m Installing zsh \e[0m"
 sudo apt install zsh
 
 echo "\e[45m changing default shell to zsh \e[0m"
-
 sudo chsh -s /usr/bin/zsh root
 sudo chsh -s /usr/bin/zsh $USER
 
 echo "\e[45m Installing oh-my-zsh \e[0m"
-
 # Install oh-my-zsh via curl
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 echo "\e[45m Installing zsh-syntax-highlighting \e[0m"
-
 # Install zsh-syntax-highlighting (not via oh-my-zsh)
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
 echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
 
 echo "\e[45m Installing zsh-autosuggestions \e[0m"
-
 # Install zsh-autosuggestions (not via oh-my-zsh)
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
 echo "source ${(q-)PWD}/zsh-autosuggestions/zsh-autosuggestion.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
